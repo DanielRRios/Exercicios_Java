62bf2ea84f9d971881bac426fa9e14b2a0e6a705
DanielRRios <daniel.rios@sptech.school>
2023-06-27 21:12:18 -0300

    Atualizacao

--- exercicios/src/exercicios/ex01.java
+++
@@ -1,9 +0,0 @@
-package exercicios;
-
-public class ex01 {
-	public static void main(String [] args) {
-		// # Exercício Java 001: Crie um prorama que escreva "Olá, mundo." na tela.
-		
-		System.out.println("Olá, mundo!");
-	}
-}
